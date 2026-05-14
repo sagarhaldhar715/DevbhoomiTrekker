d088131903d234a96a35da6d8482e13d633b10c7
Nikita Kumari <nikitatiwary05@gmail.com>
2026-05-14 22:49:05 +0530

    add models

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
